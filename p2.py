11fa764e78b8bbe3bfbbbeb20b2f00e2582cda7f
Henry Lin <henry.lin@appdirect.com>
2018-12-14 05:17:06 -0800

    v2  push

--- p2.py
+++ p2.py
@@ -5,3 +5,33 @@ def fib(n):
   a,b = b,a+b
  return a
 print fib(5)
+
+## Example 2: Using recursion
+def fibR(n):
+ if n==1 or n==2:
+  return 1
+ return fibR(n-1)+fibR(n-2)
+print fibR(5)
+
+
+## Example 3: Using generators
+a,b = 0,1
+def fibI():
+ global a,b
+ while True:
+  a,b = b, a+b
+  yield a
+f=fibI()
+f.next()
+f.next()
+f.next()
+f.next()
+print f.next()
+
+
+## Example 4: Using memoization
+def memoize(fn, arg):
+ memo = {}
+ if arg not in memo:
+  memo[arg] = fn(arg)
+  return memo[arg]
